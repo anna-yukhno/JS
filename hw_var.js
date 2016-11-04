77642d8c9cfd7e99a1e642b224a07d3d318eed78
Anna <anuta.praga@gmail.com>
2016-11-04 11:05:06 +0200

    tested version of puzirek

--- hw_var.js
+++ hw_var.js
@@ -1,9 +1,11 @@
 var tmp, i, j;
 var arr = [1, 4, 3, 6, 2, 5];
 
-for (var i = 0; i = arr.length - 1; i++) {
-	for ( j = i+1; j = (arr.length - 1); j++) {
-		if (arr[i] > arr[j]) {
+console.log('number combination: ' + arr);
+
+for (var i = 0; i <= arr.length - 1; i++) {
+	for ( j = i+1; j <= (arr.length - 1); j++) {
+		if (arr[i] < arr[j]) {
 		tmp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = tmp;
@@ -11,3 +13,6 @@ for (var i = 0; i = arr.length - 1; i++) {
 	}
 }
 console.log(arr);
+console.log(arr[0] *= 1000);
+console.log(arr[1] *= 100);
+console.log(arr[2] *= 10);
