5e1cf82a144c5963203e232cbcda8acbd1e074f4
Anna <anuta.praga@gmail.com>
2016-11-08 16:57:22 +0200

    now it is not hardcode

--- hw_05_11.js
+++ hw_05_11.js
@@ -5,18 +5,21 @@ function rand (min, max) {
 var arr = [];
 var arr2 = [];
 
-for (var i = 0; i < 30 ; i++) {
+for (var i = 1; i <= 30; i++) {
 	if ((i%2) === 0) {
 		arr.push(rand(0, 100));
 	} else {
 		arr.push(rand(-100, 0));
 	}
-}
-console.log(arr);
-
-for (var i = 0; i <= arr.length ; i++) {
 	if ((i != 0) && (i % 5) === 0) {
 		arr2.push(arr[i-1]);
 	}
 }
+console.log(arr);
+
+// for (var i = 0; i <= arr.length ; i+=5) {
+// 	if ((i != 0) && (i % 5) === 0) {
+// 		arr2.push(arr[i-1]);
+// 	}
+// }
 console.log(arr2);
